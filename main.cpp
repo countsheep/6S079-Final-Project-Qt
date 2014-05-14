91c5a85e5906a71060c7b541bc4940b1bd55811f
jhaip <jhaip@mit.edu>
2014-05-14 00:27:21 -0400

    printing facet groups

--- main.cpp
+++ main.cpp
@@ -27,6 +27,8 @@
 
 #include <stdio.h>
 
+#include <QtDebug>
+
 // Mesh segmentation typedefs:
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef Kernel::Point_3 Point_3;
@@ -47,11 +49,33 @@ typedef K::Triangle_3               Triangle;
 
 Polyhedron mesh;
 
+void cgalstuffdoit() {
+    Polyhedron mesh;
+    std::ifstream input("/Users/jhaip/Desktop/segment2/data/cactus.off");
+    if ( !input || !(input >> mesh) || mesh.empty() ) {
+        qDebug() << "Not a valid off file.";
+        //return EXIT_FAILURE;
+    }
+    // create a property-map for segment-ids
+    typedef std::map<Polyhedron::Facet_const_handle, std::size_t> Facet_int_map;
+    Facet_int_map internal_segment_map;
+    boost::associative_property_map<Facet_int_map> segment_property_map(internal_segment_map);
+    // calculate SDF values and segment the mesh using default parameters.
+    size_t number_of_segments = CGAL::segmentation_via_sdf_values(mesh, segment_property_map);
+    qDebug() << "Number of segments: " << number_of_segments;
+
+    for(Facet_iterator facet_it = mesh.facets_begin(); facet_it != mesh.facets_end(); ++facet_it) {
+        qDebug() << segment_property_map[facet_it] << " ";
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    cgalstuffdoit();
 
     return a.exec();
 }
